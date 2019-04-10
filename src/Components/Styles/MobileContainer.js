5428f6f55b9e30e6480d13beb72c4c72a3f927a7
Eric Nielsen <ericnielsen@Erics-MacBook-Pro-3.local>
2019-04-10 13:24:54 -0600

    moble box added to components

---
+++ src/Components/Styles/MobileContainer.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export default styled.section`
+    display: flex;
+	flex-direction: column;
+    align-items: center;
+    background: red;
+    margin: 5px;
+    border-radius: 5px;
+`;
