0f84a9efdc3f8b4a4e8822172a4f9e96941f70ea
Eric Nielsen <ericnielsen@Erics-MacBook-Pro-3.local>
2019-04-10 15:17:12 -0600

    pre axios search bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import "./App.css"
 import routes from "./routes"
 import Nav from "./Components/Nav/Nav"
 import Auth from "./utils/Auth/Auth"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faSearch)
 
 class App extends Component {
   render() {
--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import styled from 'styled-components'
+import axios from 'axios';
+
+const SearchContainer = styled.div`
+    position: relative;
+    height: fit-content;
+    padding: 5px;
+`;
+const SearchIcon = styled.div`
+    position: relative;
+    top:1%;
+    left: 1%;
+    width: fit-content;
+`;
+
 
 const Search = () => {
+
+    const [searchInput, setSearchInput] = useState('')
+    const [openSearch, setOpenSearch] = useState(false)
+    console.log(searchInput)
+
+    const fireSearch = () => {
+        
+    }
+
     return(
-        <div>
-            <h1>Search</h1>
+        <div style={{background:'grey'}}>
+            <SearchContainer>
+                <SearchIcon>
+                    <FontAwesomeIcon icon="search" onClick={_ => setOpenSearch(!openSearch)}/>
+                </SearchIcon>
+                    {openSearch &&
+                        <input onKeyDown={fireSearch} val={searchInput} onChange={e => setSearchInput(e.target.value)} placeholder='Search' />
+                    }
+            </SearchContainer>
         </div>
     );
 };
