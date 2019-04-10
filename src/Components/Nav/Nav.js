d2db16865a91586324d302bd690555ce37cf4afb
Panda L <jbvhl@hotmail.com>
2019-04-10 11:18:30 -0600

    merging

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,7 +1,55 @@
 import React from "react"
 
-function Nav() {
-  return <div />
+//MaterialUI
+import PropTypes from "prop-types"
+import { withStyles } from "@material-ui/core/styles"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import Button from "@material-ui/core/Button"
+import IconButton from "@material-ui/core/IconButton"
+import MenuIcon from "@material-ui/icons/Menu"
+
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
 }
 
-export default Nav
+function Nav(props) {
+  const { classes } = props
+  return (
+    <div className={classes.root}>
+      <AppBar className={classes.MuiToolbar} position="static">
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            GameHub
+          </Typography>
+          <Button onClick={props.auth.login} color="inherit">
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+Nav.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Nav)
