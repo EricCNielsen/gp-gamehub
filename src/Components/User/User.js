4dbd7c32c81c8d801f953db8917c8179baa1b313
Panda L <jbvhl@hotmail.com>
2019-04-10 10:40:58 -0600

    starting user

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import {v4 as randomString} from 'uuid';
+import Dropzone from 'react-dropzone';
+import {GridLoader} from 'react-spinners';
 
 const User = () => {
+    const [isUploading, setUploading] = useState(false)
     return(
         <div>
             <h1>User</h1>
