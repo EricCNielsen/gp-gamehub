99dc575f3e92683de917f373bb5ab6bf9d761022
Panda L <jbvhl@hotmail.com>
2019-04-11 15:33:11 -0600

    update endpoint

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -14,5 +14,28 @@ module.exports = {
         // }
 
         res.status(200).send(searchResults)
-    }
+    },
+    updateUser: async (req, res) => {
+        try {
+          const { user_id, username, email, location, picture, bio } = req.body;
+          console.log(11, req.body);
+          //   const { id } = req.session.user;
+          const db = req.app.get("db");
+          let user = await db.updateUser({
+            user_id,
+            username,
+            email,
+            location,
+            picture,
+            bio
+          });
+          console.log(22, user);
+          //   user = user[0];
+          //   req.session.user = user;
+          res.status(200).send(user);
+        } catch (error) {
+          console.log("error updating user:", error);
+          res.status(500).send("error updating user");
+        }
+      }
 };
