2166bfb9531d288ab50d211af8314bbb019e5624
Eric Nielsen <ericnielsen@Erics-MacBook-Pro-3.local>
2019-04-11 15:49:34 -0600

    functional search bar!!!

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -3,11 +3,10 @@ module.exports = {
         const db = req.app.get('db');
         const {search} = req.query
         console.log(111, search)
-
+        let searchResults = {}
         
-        let searchResultUsers = await db.get_users(search)
-        let searchResultClans = await db.get_clans(search)
-        let searchResults = [searchResultUsers, searchResultClans]
+        searchResults.users = await db.get_users(search)
+        searchResults.clans = await db.get_clans(search)
         
         // if (searchResults === []){
         //     return res.status(500).send('Nothing was found...')
---
+++ src/Components/Search/SearchResults/UsersResults.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const User = styled.div`
+    background-color: rgba(229,229,229, .9) ;
+    border: 2px solid black;
+    border-radius: 10px;
+    margin-top: 2vh;
+    margin-left: 10vw;
+    margin-right: 10vw;
+    height: fit-content;
+    img {
+        border: 1px solid black;
+        border-radius: 50%;
+        height:10vh;
+        width:10vh;
+        object-fit: cover
+    }
+    
+`
+
+
+const UserResults = ({users}) => {
+     const results = users
+     .map((user, i) => {
+        return(
+            <User key={i}>
+                <h3>Users</h3>
+                <hr/>
+                <img src={user.picture} alt="profile picture"/>
+                <h1>{user.username}</h1>
+                <h2>{user.location}</h2>
+            </User>
+        )
+    })
+    return (
+        <div>
+            {results}
+        </div>
+    )
+}
+
+export default UserResults
