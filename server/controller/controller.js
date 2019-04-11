456e0b46a1a999cd7e18b38951719be8dbd340bc
William Scholl <willscholl@me.com>
2019-04-11 15:42:24 -0600

    landing finished for mvp

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,18 +1,17 @@
 module.exports = {
-    search: async (req, res) => {
-        const db = req.app.get('db');
-        const {search} = req.query
-        console.log(111, search)
+  search: async (req, res) => {
+    const db = req.app.get("db");
+    const { search } = req.query;
+    console.log(111, search);
 
-        
-        let searchResultUsers = await db.get_users(search)
-        let searchResultClans = await db.get_clans(search)
-        let searchResults = [searchResultUsers, searchResultClans]
-        
-        // if (searchResults === []){
-        //     return res.status(500).send('Nothing was found...')
-        // }
+    let searchResultUsers = await db.get_users(search);
+    let searchResultClans = await db.get_clans(search);
+    let searchResults = [searchResultUsers, searchResultClans];
 
-        res.status(200).send(searchResults)
-    }
+    // if (searchResults === []){
+    //     return res.status(500).send('Nothing was found...')
+    // }
+
+    res.status(200).send(searchResults);
+  }
 };
