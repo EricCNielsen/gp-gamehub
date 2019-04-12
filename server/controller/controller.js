25e14bcb6119e14cd0e65e8a1ba05a74754b7e32
Jose Tobar <josectobar@gmail.com>
2019-04-11 19:42:40 -0600

    changed controller to last push from Eric

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -3,14 +3,10 @@ module.exports = {
     const db = req.app.get("db")
     const { search } = req.query
     console.log(111, search)
+    let searchResults = {}
 
-    let searchResultUsers = await db.get_users(search)
-    let searchResultClans = await db.get_clans(search)
-    let searchResults = [searchResultUsers, searchResultClans]
-
-    // if (searchResults === []){
-    //     return res.status(500).send('Nothing was found...')
-    // }
+    searchResults.users = await db.get_users(search)
+    searchResults.clans = await db.get_clans(search)
 
     res.status(200).send(searchResults)
   },
@@ -26,27 +22,11 @@ module.exports = {
       console.log(err)
     }
   },
-  search: async (req, res) => {
-    const db = req.app.get("db")
-    const { search } = req.query
-    console.log(111, search)
-
-    let searchResultUsers = await db.get_users(search)
-    let searchResultClans = await db.get_clans(search)
-    let searchResults = [searchResultUsers, searchResultClans]
-
-    // if (searchResults === []){
-    //     return res.status(500).send('Nothing was found...')
-    // }
-
-    res.status(200).send(searchResults)
-  },
   getUser: async (req, res) => {
     try {
       const { auth_id } = req.session.user
       const db = req.app.get("db")
       const user = await db.get_user(auth_id)
-      console.log(user)
       res.status(200).send(user)
     } catch (err) {
       console.log(err)
