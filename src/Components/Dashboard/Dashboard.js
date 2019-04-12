9e6f62990382c0a9fbbb3061448bf3f023370843
Jose Tobar <josectobar@gmail.com>
2019-04-11 22:53:22 -0600

    logout and user info working on Nav

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React, { useEffect } from "react"
 import Group from "../Group/Group"
 import Search from "../Search/Search"
 import GamingNews from "../GamingNews/GamingNews"
