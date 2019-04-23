774b88d1ee1fdae821bafb3886b38c8624a446aa
William Scholl <willscholl@me.com>
2019-04-23 14:00:11 -0600

    create post button added 60%

--- src/Components/Group/GroupInfo.js
+++ src/Components/Group/GroupInfo.js
@@ -7,19 +7,18 @@ import { GridLoader } from "react-spinners";
 import { updateClan } from "../../ducks/reducer";
 import { connect } from "react-redux";
 
-
-const GroupInfo = ( {clan, user_id, props})=> {
+const GroupInfo = ({ clan, user_id, props }) => {
   const [url, setUrl] = useState(""),
-        [isUploading, setUploading] = useState(""),
-        [name, setName] = useState(""),
-        [edit, setEdit] = useState(false),
-        [bio, setBio] = useState(""),
-        [clanState, setClanState] = useState([]);
+    [isUploading, setUploading] = useState(""),
+    [name, setName] = useState(""),
+    [edit, setEdit] = useState(false),
+    [bio, setBio] = useState(""),
+    [clanState, setClanState] = useState([]);
 
   let handleEdit = () => {
     setEdit(!edit);
-    setName(!name ? props.clanName : name );
-    setBio(!bio ? props.clanBio : bio );
+    setName(!name ? props.clanName : name);
+    setBio(!bio ? props.clanBio : bio);
     setUrl(url ? url : props.clanAvatar);
   };
 
@@ -74,11 +73,13 @@ const GroupInfo = ( {clan, user_id, props})=> {
   let handleSave = async () => {
     let clan = {
       clan_id: props.clan_id,
-      url: (url ? url : 'http://newsite.jmwc.org/wp-content/uploads/2016/05/group-placeholder.png'),
+      url: url
+        ? url
+        : "http://newsite.jmwc.org/wp-content/uploads/2016/05/group-placeholder.png",
       bio,
       name
     };
-    console.log("testing save", clan)
+    console.log("testing save", clan);
     try {
       let res = await axios.put("/api/clan", clan);
       console.log("handleSave", res.data);
@@ -89,67 +90,68 @@ const GroupInfo = ( {clan, user_id, props})=> {
     }
   };
 
-  console.log(props)
-    return (
-        
-          <div className="clanInfo">
-            {edit ? (
-              <>
-                <img className="logo" src={ url ? url : props.clanAvatar } alt="logo" />
+  // console.log(props)
+  return (
+    <div className="clanInfo">
+      {edit ? (
+        <>
+          <img className="logo" src={url ? url : props.clanAvatar} alt="logo" />
 
-                {url ? (
-                  <button onClick={() => setUrl("")}>edit</button>
-                ) : (
-                  <div className="dropzone">
-                    <Dropzone
-                      className="dropzone"
-                      onDropAccepted={getSignedRequest}
-                      accept="image/*"
-                      multiple={false}  
-                    >
-                      {isUploading ? (
-                        <GridLoader />
-                      ) : (
-                        <p>Drop File or Click Here</p>
-                      )}
-                    </Dropzone>
-                  </div>
-                )}
-                <br />
-                <input
-                  id="name"
-                  placeholder={props.clanName ? props.clanName : "Clan Name"}
-                  onFocus={e => e.target.placeholder=""}
-                  onBlur={e => e.target.placeholder= props.clanName ? props.clanName : "Clan Name"}
-                  value={ !name ? props.clanName : name }
-                  onChange={e => setName(e.target.value)}
-                  style={{textAlign:"center"}}
-                />
-                <textarea
-                  id="bio"
-                  placeholder={props.clanBio ? props.clanBio : "Clan Bio"}
-                  onFocus={e => e.target.placeholder=""}
-                  onBlur={e => e.target.placeholder= props.clanBio ? props.clanBio : "Clan Bio"}
-                  value={bio}
-                  onChange={e => setBio(e.target.value)}
-                />
-                <button onClick={handleSave}>Save</button>
-              </>
-            ) : (
-              <>
-                <img className="logo" src={props.clanAvatar} alt="logo" />
-                <h1>{props.clanName}</h1>
-                <p id="bio">{props.clanBio}</p>
-                {props.user_id === props.owner_id ? (
-                  <button onClick={handleEdit}>Edit</button>
-                ) : null}
-              </>
-            )}
-          </div>
-    )
-  } 
-
-  
+          {url ? (
+            <button onClick={() => setUrl("")}>edit</button>
+          ) : (
+            <div className="dropzone">
+              <Dropzone
+                className="dropzone"
+                onDropAccepted={getSignedRequest}
+                accept="image/*"
+                multiple={false}
+              >
+                {isUploading ? <GridLoader /> : <p>Drop File or Click Here</p>}
+              </Dropzone>
+            </div>
+          )}
+          <br />
+          <input
+            id="name"
+            placeholder={props.clanName ? props.clanName : "Clan Name"}
+            onFocus={e => (e.target.placeholder = "")}
+            onBlur={e =>
+              (e.target.placeholder = props.clanName
+                ? props.clanName
+                : "Clan Name")
+            }
+            value={!name ? props.clanName : name}
+            onChange={e => setName(e.target.value)}
+            style={{ textAlign: "center" }}
+          />
+          <textarea
+            id="bio"
+            placeholder={props.clanBio ? props.clanBio : "Clan Bio"}
+            onFocus={e => (e.target.placeholder = "")}
+            onBlur={e =>
+              (e.target.placeholder = props.clanBio
+                ? props.clanBio
+                : "Clan Bio")
+            }
+            value={bio}
+            onChange={e => setBio(e.target.value)}
+          />
+          <button onClick={handleSave}>Save</button>
+        </>
+      ) : (
+        <>
+          <img className="logo" src={props.clanAvatar} alt="logo" />
+          <h1>{props.clanName}</h1>
+          <p id="bio">{props.clanBio}</p>
+          {props.user_id === props.owner_id ? (
+            <button onClick={handleEdit}>Edit</button>
+          ) : null}
+        </>
+      )}
+    </div>
+  );
+};
 
 const mapStateToProps = reduxState => {
   return {
@@ -162,4 +164,7 @@ const mapStateToProps = reduxState => {
   };
 };
 
-export default connect(mapStateToProps,{ updateClan })(GroupInfo);
+export default connect(
+  mapStateToProps,
+  { updateClan }
+)(GroupInfo);
--- src/Components/Group/InGroupMini.js
+++ src/Components/Group/InGroupMini.js
@@ -1,25 +1,77 @@
-import React from "react";
-import './group.css';
+import React, { useState } from "react";
+import "./group.css";
 import styled from "styled-components";
 import { connect } from "react-redux";
 import { updateClan } from "./../../ducks/reducer";
 import PostViewer from "./../PostsViewer/PostViewer";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 const ClanMiniContainer = styled.div`
   background-color: white;
   margin: 0;
-`
+`;
 
-const InGroupMini = (props) => {
-  console.log(props)
+const InGroupMini = props => {
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  console.log(props);
+  const { classes } = props;
   return (
     <div className="feed">
-      <h1 style={{textAlign:"left", paddingLeft:"3%"}}>{props.clanName}</h1>
-      <hr/>
-      <PostViewer id={props.clan_id}/>
+      <div>
+        <h1 style={{ textAlign: "left", paddingLeft: "3%" }}>
+          {props.clanName}
+        </h1>
+        <Button variant="outlined" color="secondary" onClick={handleClickOpen}>
+          Create a post
+        </Button>
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To subscribe to this website, please enter your email address
+              here. We will send updates occasionally.
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Email Address"
+              type="email"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleClose} color="primary">
+              Subscribe
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+      <hr />
+      <PostViewer id={props.clan_id} />
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = reduxState => {
   return {
@@ -32,4 +84,7 @@ const mapStateToProps = reduxState => {
   };
 };
 
-export default connect(mapStateToProps, { updateClan })(InGroupMini)
+export default connect(
+  mapStateToProps,
+  { updateClan }
+)(InGroupMini);
